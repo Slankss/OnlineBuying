38fe7a792c9558cabdae558cd36140bbc4685206
okankkl <okankukull@gmail.com>
2023-07-07 17:40:14 +0300

    Login page completed

--- app/src/main/java/com/example/onlinebuying/View/LoginPage.kt
+++ app/src/main/java/com/example/onlinebuying/View/LoginPage.kt
@@ -1,13 +1,51 @@
 package com.example.onlinebuying.View
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.example.onlinebuying.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.onlinebuying.Repository.FirebaseRepository
+import com.example.onlinebuying.Widgets.CustomButton
+import com.example.onlinebuying.Widgets.CustomSnackkBar
+import com.example.onlinebuying.Widgets.PasswordOutlinedTextField
+import com.example.onlinebuying.Widgets.UsernameOutlinedTextField
+import com.example.onlinebuying.ui.theme.Navy
+import com.example.onlinebuying.ui.theme.Orange
+import com.example.onlinebuying.ui.theme.Red
+import com.example.onlinebuying.ui.theme.Teal
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,18 +53,188 @@ import com.example.onlinebuying.Repository.FirebaseRepository
 fun LoginPage(
     navController: NavController,
     firebaseRepository: FirebaseRepository
-    ){
+){
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    // import androidx.compose.runtime.getValue
+    // import androidx.compose.runtime.setValue
+    // bunları importlaman lazım aşağıdaki gibi kullanman için
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    var usernameErrorState by remember { mutableStateOf(false) }
+    var passwordErrorState by remember { mutableStateOf(false) }
+
+    var passwordVisibility by remember { mutableStateOf(false) }
 
     Scaffold(
-
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState){ snackBarData ->
+                CustomSnackkBar(
+                    message = snackBarData.visuals.message,
+                    containerColor = Teal,
+                    icon = null
+                )
+            }
+        }
     ) {
 
-        Column {
-            Text(text = "LOGIN")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = Orange
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                Color.White,
+                                Color.White,
+                                Orange
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+                    .weight(5f)
+            ){
+                Image(
+                    painterResource(
+                        id = R.drawable.social_engagement
+                    ),
+                    contentDescription = "Image",
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                )
+
+            }
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(5f),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 7.dp,
+                ),
+                shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                )
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 25.dp, end = 25.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    UsernameOutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = username,
+                        labelText = "Kullanıcı adı",
+                        errorState = usernameErrorState,
+                        iconClick =
+                        {
+                            username = ""
+                        },
+                        onValueChange = {
+                            usernameErrorState = false
+                            username = it
+                        }
+                    )
+
+                    PasswordOutlinedTextField(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        text = password,
+                        labelText = "Parola",
+                        errorState = passwordErrorState,
+                        passwordVisibility = passwordVisibility,
+                        iconClick =
+                        {
+                            passwordVisibility = !passwordVisibility
+                        },
+                        onValueChange = {
+                            passwordErrorState = false
+                            password = it
+                        }
+                    )
+
+                    CustomButton(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(14.dp),
+                        text = "Giriş Yap",
+                        fontSize = 18,
+                        textColor = Color.White,
+                        containerColor = Red,
+                        iconId = null,
+                        onClick = {
+                            check(username.trim(),password.trim()){ isUsernameEmpty,isPasswordEmpty ->
+                                usernameErrorState = isUsernameEmpty
+                                passwordErrorState = isPasswordEmpty
+
+                                if(!isUsernameEmpty && !isPasswordEmpty){
+                                    // login işlemleri yapılabilir
+                                    scope.launch {
+                                        snackbarHostState.showSnackbar(
+                                            message =  "Başarıyla giriş yaptınız"
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    )
+
+                    Row(
+                        modifier = Modifier,
+                        horizontalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        Text(
+                            text = "Hesabın yok mu?",
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                        Text(
+                            text = "Kayıt ol",
+                            color = Red,
+                            style = MaterialTheme.typography.labelMedium,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .clickable {
+
+                                }
+
+                        )
+                    }
+                }
+            }
         }
 
     }
 
+}
 
+fun check(username  :String,password : String,
+          errorListener : (Boolean,Boolean) -> Unit)
+{
+
+    var isUsernameEmpty = false
+    var isPasswordEmpty = false
+
+    if(username.isBlank()){
+        isUsernameEmpty = true
+    }
+    if(password.isBlank()){
+        isPasswordEmpty = true
+    }
+    errorListener(isUsernameEmpty,isPasswordEmpty)
 
 }
--- app/src/main/java/com/example/onlinebuying/Widgets/Widgets.kt
+++ app/src/main/java/com/example/onlinebuying/Widgets/Widgets.kt
@@ -1,23 +1,41 @@
 package com.example.onlinebuying.Widgets
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.onlinebuying.R
+import com.example.onlinebuying.ui.theme.Orange
+import com.example.onlinebuying.ui.theme.Teal
 
 @Composable
 fun CustomButton(modifier : Modifier,
@@ -68,6 +86,165 @@ fun CustomButton(modifier : Modifier,
                 color = textColor,
                 fontSize = fontSize.sp
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UsernameOutlinedTextField(
+    modifier: Modifier,
+    text : String,
+    labelText : String,
+    errorState : Boolean,
+    iconClick : () -> Unit,
+    onValueChange : (String) -> Unit
+){
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = text,
+        shape = RoundedCornerShape(8.dp),
+        onValueChange = {
+            onValueChange(it)
+        },
+        label = {
+            Text(
+                text = labelText,
+                color = Color.Black
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Black,
+            errorLabelColor = Color.Red,
+            cursorColor = Color.Black,
+            containerColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.DarkGray,
+            focusedIndicatorColor = Teal
+
+        ),
+        trailingIcon = {
+            IconButton(
+                onClick =
+                {
+                    iconClick()
+                }
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = "Tralaiding icon"
+                )
+            }
+        },
+        singleLine = true,
+        isError = errorState,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Ascii
+        ),
+        textStyle = MaterialTheme.typography.bodyMedium
+
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PasswordOutlinedTextField(
+    modifier: Modifier,
+    text : String,
+    labelText : String,
+    errorState : Boolean,
+    passwordVisibility : Boolean?,
+    iconClick : () -> Unit,
+    onValueChange : (String) -> Unit
+){
+
+    OutlinedTextField(
+        modifier = modifier,
+        value = text,
+        shape = RoundedCornerShape(8.dp),
+        onValueChange = {
+            onValueChange(it)
+        },
+        label = {
+            Text(
+                text = labelText,
+                color = Color.Black
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Black,
+            errorLabelColor = Color.Red,
+            cursorColor = Color.Black,
+            containerColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Black,
+            focusedIndicatorColor = Teal
+
+        ),
+        visualTransformation =
+        if(passwordVisibility == null || passwordVisibility) VisualTransformation.None
+        else PasswordVisualTransformation(),
+        trailingIcon = {
+            if(passwordVisibility != null){
+                IconButton(
+                    onClick =
+                    {
+                        iconClick()
+                    }
+                ) {
+
+                    Icon(
+                        painter = painterResource(
+                            id = when(passwordVisibility){
+                                true -> R.drawable.ic_visibility_off
+                                false -> R.drawable.ic_visibility_on
+                            }
+                        ),
+                        contentDescription = "Tralaiding icon"
+                    )
+                }
+            }
+
+        },
+        singleLine = true,
+        isError = errorState,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Ascii
+        ),
+        textStyle = MaterialTheme.typography.bodyMedium
+
+    )
+
+}
+
+@Composable
+fun CustomSnackkBar(
+    message : String,
+    containerColor : Color,
+    icon : Icon?
+){
+
+    Card(
+        modifier = Modifier
+            .padding(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 7.dp
+        )
+    ){
+        Snackbar(
+            modifier = Modifier,
+            containerColor = containerColor
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(
+                    text = message,
+                    style =  MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
 }
